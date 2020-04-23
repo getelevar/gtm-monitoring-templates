2e96b0577c30140092ebd0ebe7fd3d1d1aca049a
Aaron Hakala <aaron.hakala@gmail.com>
2020-04-23 10:15:58 +0200

    add small requested changes to git-all script

--- scripts/git-all.sh
+++ scripts/git-all.sh
@@ -13,21 +13,24 @@ do
             find ./templates -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} push \;
             ;;
         "add")
-            echo What do you want to add?
+            echo "What do you want to add?"
             read files_to_add
             find ./templates -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} add $files_to_add \;
             ;;
         "commit")
-            echo What is the commit message?
+            echo "What is the commit message?"
             read commit_msg
 
             find ./templates -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} commit -m "$commit_msg" \;
             ;;
         "checkout")
-            echo What branch do you want to checkout to?
+            echo "What branch do you want to checkout to?"
             read branch_name
             find ./templates -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} checkout $branch_name \;
             ;;
-        *) echo "invalid option $REPLY";;
+        *)
+            echo "invalid option $REPLY"
+            break
+            ;;
     esac
 done
